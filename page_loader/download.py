c02b806d09ea3db3381f95651b668f90a32d2927
Aleksandr <nastyavin86@br.ru>
2022-05-26 19:45:20 +0300

    changed page_loader/download.py

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,5 +1,19 @@
 import os
+import requests
 
 
-def download(url, filepath, output):
-    return "Hello"
+def download(url, filepath=os.getcwd()):
+    new_path = name_formation(url)
+    filename = os.path.join(filepath, new_path)
+    with open(new_path, 'w') as f:
+        r = requests.get(url)
+        f.write(r.text)
+    return os.path.abspath(filename)
+
+
+def name_formation(addres):
+    path = os.path.splitext(addres.split('//')[1])[0]
+    new_path = ''
+    for sing in path:
+        new_path += sing if sing.isalnum() else sing.replace(sing, '-')
+    return new_path + '.html'
