f9f511c7edf97c3f4fa40da6b894474b7facc8ff
Aleksandr <nastyavin86@br.ru>
2022-05-29 20:57:32 +0300

    modified page_loader/download.py

--- page_loader/download.py
+++ page_loader/download.py
@@ -8,12 +8,14 @@ def download(url, filepath=os.getcwd()):
     with open(new_path, 'w') as f:
         r = requests.get(url)
         f.write(r.text)
+    name_dir = name_formation(url, '_file')
+    new_dir = os.mkdir(name_dir)
     return os.path.abspath(filename)
 
 
-def name_formation(addres):
+def name_formation(addres, suffix='.html'):
     path = os.path.splitext(addres.split('//')[1])[0]
     new_path = ''
     for sing in path:
         new_path += sing if sing.isalnum() else sing.replace(sing, '-')
-    return new_path + '.html'
+    return new_path + suffix
