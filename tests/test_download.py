fefa42f00447226a060da2e5bce9e0ce76d66706
Aleksandr <nastyavin86@br.ru>
2022-05-29 20:59:28 +0300

    modified tests/test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -5,16 +5,13 @@ import requests
 from page_loader.download import download
 
 
-correct_name = 'en-wikipedia-org-wiki-Main-Page.html'
-addres = 'https://en.wikipedia.org/wiki/Main_Page'
+correct_name = 'en-wikipedia.html'
+addres = 'https://en.wikipedia.org'
 
 
-def test_download():
+def test_download(make_request, read_file):
     with tempfile.TemporaryDirectory() as temp:
         random_path = os.path.join(temp, correct_name)
-        with requests_mock.Mocker() as mock:
-            mock.get(addres, text='hello')
-            responce = requests.get(addres)
-            result = download(addres, temp)
-    assert random_path == result
-    assert responce.text == 'hello'
+        result = download(addres, temp)
+        assert random_path == result
+        assert make_request == read_file
