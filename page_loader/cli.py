3b817cd9833d4e4c7064969d050f8b1c0348f16c
Aleksandr <nastyavin86@br.ru>
2022-05-29 20:57:00 +0300

    modified page_loader/cli.py

---
+++ page_loader/cli.py
@@ -0,0 +1,11 @@
+import argparse
+import os
+
+def make_cli():
+    parser = argparse.ArgumentParser(prog='page-loader',
+                                     description='Page loader')
+    parser.add_argument('url')
+    parser.add_argument('-o', '--output', default=os.getcwd(),
+                        help='sets the program launch directory')
+    args = parser.parse_args()
+    return args.url, args.output
