55069ae43c4c59bce5b40776e6ff5484bb9291c3
Aleksandr <nastyavin86@br.ru>
2022-05-29 20:57:57 +0300

    modified page_loader/scripts/page_loader.py

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 
-import argparse
-import os
+from page_loader.cli import make_cli
 from page_loader.download import download
 
 
 def main():
-    parser = argparse.ArgumentParser(prog='page-loader',
-                                     description='Page loader')
-    parser.add_argument('url')
-    parser.add_argument('-o', '--output', default=os.getcwd(),
-                        help='sets the program launch directory')
-    args = parser.parse_args()
-    print(download(args.url, args.output))
+    arg_one, arg_two = make_cli()
+    print(download(arg_one, arg_two))
 
 
 if __name__ == '__main__':
