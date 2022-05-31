717476ce92cdde9f9631b006254655ffeaedf36f
Aleksandr <nastyavin86@br.ru>
2022-05-31 19:06:53 +0300

    modified page_loader/loading.py

--- page_loader/loading.py
+++ page_loader/loading.py
@@ -2,10 +2,20 @@ import os
 import requests
 
 
-def loading(url, file_path):
-    file_name = name_formation(url)
-    new_path = os.path.join(file_path, file_name)
-    with open(file_name, 'w') as fp:
-        request = requests.get(url)
-        fp.write(request.text)
-    return filename
+def loading(url, file_name, mode='w'):
+    response = requests.get(url)
+    if mode == 'wb':
+        with open(file_name, 'wb') as fp:
+            fp.write(response.content)
+    if mode == 'w':
+        with open(file_name, 'w') as fp:
+            fp.write(response.text)
+
+
+def name_formation(addres, suffix='.html'):
+    path = os.path.splitext(addres.split('//')[1])[0]
+    new_path = ''
+    for sing in path:
+        new_path += sing if sing.isalnum() else sing.replace(sing, '-')
+    return new_path + suffix
+    
