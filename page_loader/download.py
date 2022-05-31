fc30ac8cb85521b76e6a2c1ad9465f9d397d563c
Aleksandr <nastyavin86@br.ru>
2022-05-31 19:06:18 +0300

    modified page_loader/download.py

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,21 +1,10 @@
 import os
-import requests
-
+from page_loader.loading import loading, name_formation
 
 def download(url, filepath=os.getcwd()):
-    new_path = name_formation(url)
-    filename = os.path.join(filepath, new_path)
-    with open(new_path, 'w') as f:
-        r = requests.get(url)
-        f.write(r.text)
+    file_name = name_formation(url)
+    loading(url, file_name, 'w')
+    filename = os.path.join(filepath, file_name)
     name_dir = name_formation(url, '_file')
     new_dir = os.mkdir(name_dir)
     return os.path.abspath(filename)
-
-
-def name_formation(addres, suffix='.html'):
-    path = os.path.splitext(addres.split('//')[1])[0]
-    new_path = ''
-    for sing in path:
-        new_path += sing if sing.isalnum() else sing.replace(sing, '-')
-    return new_path + suffix
