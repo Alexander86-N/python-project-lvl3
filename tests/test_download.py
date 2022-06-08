72e00fc56e449315c9a6559b6fe2a8c952a461c8
Aleksandr <nastyavin86@br.ru>
2022-06-08 19:21:22 +0300

    modified tests/test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,17 +1,22 @@
 import tempfile
 import os
-import requests_mock
-import requests
 from page_loader.download import download
 
 
 correct_name = 'en-wikipedia.html'
 addres = 'https://en.wikipedia.org'
+img_addres = 'https://en.wikipedia.org/assets/professions/nodejs.png'
+correct_name_dir = 'en-wikipedia_file'
 
 
-def test_download(make_request, read_file):
+def test_download(read_file, requests_mock):
     with tempfile.TemporaryDirectory() as temp:
         random_path = os.path.join(temp, correct_name)
-        result = download(addres, temp)
+        requests_mock.get('https://en.wikipedia.org', text=read_file, 
+                          headers={'Content-Type': 'text/html'})
+        requests_mock.get('https://en.wikipedia.org/assets/professions/nodejs.png', text=read_file,
+                          headers={'Content-Type': 'all'})
+        result = download('https://en.wikipedia.org', temp)
         assert random_path == result
-        assert make_request == read_file
+        assert os.path.isdir(correct_name_dir)
+        assert os.path.isfile('en-wikipedia_file/-assets-professions-nodejs.png')
