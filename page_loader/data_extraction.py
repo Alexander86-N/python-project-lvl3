c9fd2af341ba3aab31f71efe1b11d2fc1dacf7cc
Aleksandr <nastyavin86@br.ru>
2022-06-08 19:20:00 +0300

    modified page_loader/data_extraction.py

--- page_loader/data_extraction.py
+++ page_loader/data_extraction.py
@@ -1,25 +1,42 @@
 import os
+import requests
 from bs4 import BeautifulSoup
-from urllib.parse import urljoin, urlparse
-from page_loader.loading import loading, name_formation
+from urllib.parse import urlparse
 
-TAGS = {'tag': 'img', 'url': 'scr'}
 
-def data_extraction(url, file, directory):
-    with open(file, 'r') as f:
-        content = f.read()
-        soup = BeautifulSoup(content, 'html.parser')
-        for element in soup.find_all(TAGS['tag']):
-            addres = TAGS['tag'].attrs.get('src')
-            parsed = urlparse(addres)
-            if parsed.netoc and pared.netoc != url.netoc:
-                continue
-            else:
-                new_url, suffix = highlight_url_and_suffix(addres)
-                file_name = name_formation(new_url, suffix)
-                loading(addres, file_name, 'wb')
-                TAGS['tag'].attrs.['src'] = urljoin(directory, file_name)
+TAGS = {'tag': 'img', 'type': 'src'}
 
+
+def resource_extraction(url, data, directory):
+    resource_lst = []
+    soup = BeautifulSoup(data, 'html.parser')
+    for element in soup.find_all(TAGS['tag']):
+        addres = element.attrs.get(TAGS['type'])
+        if not addres:
+            continue
+        addres_pars = urlparse(addres)
+        url_pars = urlparse(url)
+        if addres_pars.netloc and addres_pars.netloc != url_pars.netloc:
+            continue
+        else:
+            new_url, suffix = highlight_url_and_suffix(addres)
+            name = name_formation(new_url, suffix)
+            print(element.attrs[TAGS['type']])
+            new_url = f'{url_pars.scheme}://{url_pars.netloc}{addres}'
+            element.attrs[TAGS['type']] = f'{directory}/{name}'
+            resource_lst.append({'addres': new_url,
+                                 'name': element.attrs[TAGS['type']]})
+            print(element.attrs[TAGS['type']])
+    return resource_lst, soup.prettify()
+
+
+def extraction_data(url):
+    response = requests.get(url)
+    if response.headers['Content-Type'] == 'text/html':
+        response.encoding = 'utf-8'
+        return response.text
+    else:
+        return response.content
 
 
 def highlight_url_and_suffix(url):
@@ -28,4 +45,12 @@ def highlight_url_and_suffix(url):
         new_url = url[:position]
         return new_url, os.path.splitext(new_url)[1]
     else:
-        return url, os.path.splitext(new_url)[1]
+        return url, os.path.splitext(url)[1]
+
+
+def name_formation(addres, suffix='.html'):
+    path_parse = urlparse(addres)
+    path = os.path.splitext(path_parse.netloc + path_parse.path)[0]
+    result = ''.join([sing if sing.isalnum() else sing.replace(sing, '-')
+                      for sing in path]) + suffix
+    return result
