c36ebf23d6c3ebd8648d38308463fd82d9b3a639
Aleksandr <nastyavin86@br.ru>
2022-06-08 19:20:29 +0300

    modified page_loader/download.py

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,10 +1,36 @@
 import os
-from page_loader.loading import loading, name_formation
+from urllib.parse import urlparse
+from page_loader.data_extraction import resource_extraction, extraction_data
+
 
 def download(url, filepath=os.getcwd()):
-    file_name = name_formation(url)
-    loading(url, file_name, 'w')
-    filename = os.path.join(filepath, file_name)
-    name_dir = name_formation(url, '_file')
-    new_dir = os.mkdir(name_dir)
+    title = name_formation(url)
+    filename = os.path.join(filepath, title)
+    data = extraction_data(url)
+
+    new_dir = name_formation(url, '_file')
+    os.mkdir(new_dir)
+
+    urls, text_html = resource_extraction(url, data, new_dir)
+    saving_data(text_html, title)
+    for url in urls:
+        data_url = extraction_data(url['addres'])
+        saving_data(data_url, url['name'])
     return os.path.abspath(filename)
+
+
+def saving_data(data, file_name):
+    if isinstance(data, str):
+        with open(file_name, 'w') as fp:
+            fp.write(data)
+    else:
+        with open(file_name, 'wb') as fp:
+            fp.write(data)
+
+
+def name_formation(addres, suffix='.html'):
+    path_parse = urlparse(addres)
+    path = os.path.splitext(path_parse.netloc + path_parse.path)[0]
+    result = ''.join([sing if sing.isalnum() else sing.replace(sing, '-')
+                      for sing in path]) + suffix
+    return result
