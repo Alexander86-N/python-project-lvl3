743b837923fc24eb45c6bab498251f0f853a1a35
Aleksandr <nastyavin86@br.ru>
2022-06-12 21:44:52 +0300

    change tests/test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,22 +1,33 @@
 import tempfile
 import os
+import pathlib
 from page_loader.download import download
 
 
-correct_name = 'en-wikipedia.html'
-addres = 'https://en.wikipedia.org'
-img_addres = 'https://en.wikipedia.org/assets/professions/nodejs.png'
-correct_name_dir = 'en-wikipedia_file'
+correct_name = 'ru-hexlet-io-courses.html'
+addres = 'https://ru.hexlet.io/courses'
+img_addres = 'https://ru.hexlet.io/assets/professions/nodejs.png'
+correct_name_dir = 'ru-hexlet-io-courses_files'
 
 
-def test_download(read_file, requests_mock):
+def test_download(read_file,
+                  read_file_css,
+                  read_file_js,
+                  read_file_img,
+                  requests_mock):
     with tempfile.TemporaryDirectory() as temp:
         random_path = os.path.join(temp, correct_name)
-        requests_mock.get('https://en.wikipedia.org', text=read_file, 
+        requests_mock.get('https://ru.hexlet.io/courses', text=read_file, 
                           headers={'Content-Type': 'text/html'})
-        requests_mock.get('https://en.wikipedia.org/assets/professions/nodejs.png', text=read_file,
-                          headers={'Content-Type': 'all'})
-        result = download('https://en.wikipedia.org', temp)
+        requests_mock.get('https://ru.hexlet.io/assets/professions/nodejs.png',
+                          content=read_file_img, headers={'Content-Type': 'all'})
+        requests_mock.get('https://ru.hexlet.io/assets/application.css',
+                          content=read_file_css, headers={'content-type': 'all'})
+        requests_mock.get('https://ru.hexlet.io/courses', content=read_file_css,
+                          headers={'content-type': 'all'})
+        requests_mock.get('https://ru.hexlet.io/packs/js/runtime.js',
+                          content=read_file_js, headers={'content-type': 'all'})
+        result = download('https://ru.hexlet.io/courses', temp)
         assert random_path == result
-        assert os.path.exists(correct_name_dir)
-        assert os.path.isfile('en-wikipedia_file/-assets-professions-nodejs.png')
+        assert os.path.isdir(os.path.abspath(correct_name_dir))
+        assert os.path.isfile('ru-hexlet-io-assets-professions-nodejs.png')
