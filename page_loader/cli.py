3880d1f788f4542382d66da47049419a4fab54f5
Aleksandr <nastyavin86@br.ru>
2022-06-12 21:41:30 +0300

    change page_loader/cli.py

--- page_loader/cli.py
+++ page_loader/cli.py
@@ -6,7 +6,12 @@ def make_cli():
     parser = argparse.ArgumentParser(prog='page-loader',
                                      description='Page loader')
     parser.add_argument('url')
-    parser.add_argument('-o', '--output', default=os.getcwd(),
+    parser.add_argument('path')
+    parser.add_argument('-o', '--output', action="store_true",
                         help='sets the program launch directory')
     args = parser.parse_args()
-    return args.url, args.output
+    if args.output:
+        path = os.getcwd()
+        return args.url, path
+    else:
+        return args.url, args.path
