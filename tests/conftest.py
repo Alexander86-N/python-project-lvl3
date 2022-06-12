7063eaae6d87cdcc2a9ec75bcaef39f34da3de52
Aleksandr <nastyavin86@br.ru>
2022-06-12 21:43:14 +0300

    change tests/conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,63 @@
 import pytest
-import requests
+import tempfile
 
 
-@pytest.fixture(params=['tests/fixtures/test_html.html'])
-def read_file(request):
-    file_name = request.param
-    with open(file_name) as f:
+@pytest.fixture
+def read_file():
+    with open('tests/fixtures/test_html.html', 'r') as f:
         result = f.read()
     return result
 
 
 @pytest.fixture
-def make_request(requests_mock, read_file):
-    requests_mock.get('https://en.wikipedia.org', text=read_file,
+def read_correct_file():
+    with open('tests/fixtures/correct_html.html', 'r') as f:
+        result = f.read()
+    return result
+
+
+@pytest.fixture
+def read_file_css():
+    with open('tests/fixtures/test_file.css', 'rb') as f:
+        result = f.read()
+    return result
+
+
+@pytest.fixture
+def read_file_js():
+    with open('tests/fixtures/test_file.js', 'rb') as f:
+        result = f.read()
+    return result
+
+
+@pytest.fixture
+def read_file_img():
+    with open('tests/fixtures/test_image/test_file_img.png', 'rb') as f:
+        result = f.read()
+    return result
+
+
+@pytest.fixture
+def make_request(requests_mock,
+                 read_file,
+                 read_file_css,
+                 read_file_js,
+                 read_file_img):
+    requests_mock.get('https://ru.hexlet.io/courses', text=read_file,
                       headers={'content-type': 'text/html'})
+    requests_mock.get('https://ru.hexlet.io/assets/professions/nodejs.png',
+                      content=read_file_img, headers={'content-type': 'all'})
+    requests_mock.get('https://ru.hexlet.io/assets/application.css',
+                      content=read_file_css, headers={'content-type': 'all'})
+    requests_mock.get('https://ru.hexlet.io/courses', content=read_file_css,
+                      headers={'content-type': 'all'})
+    requests_mock.get('https://ru.hexlet.io/packs/js/runtime.js',
+                      content=read_file_js, headers={'content-type': 'all'})
     yield
+
+
+@pytest.fixture
+def new_dirict():
+    with tempfile.TemporaryDirectory() as tmp:
+        name_dir = tmp
+        yield
