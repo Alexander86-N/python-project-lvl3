f6dc37402eb38036d6f3ba78d95bbf411afcac01
Aleksandr <nastyavin86@br.ru>
2022-06-15 19:56:18 +0300

    change tests/test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,33 +1,39 @@
 import tempfile
 import os
-import pathlib
 from page_loader.download import download
 
 
 correct_name = 'ru-hexlet-io-courses.html'
 addres = 'https://ru.hexlet.io/courses'
-img_addres = 'https://ru.hexlet.io/assets/professions/nodejs.png'
-correct_name_dir = 'ru-hexlet-io-courses_files'
+correct_name_dir = 'ru-hexlet-io-courses_file'
+file_name = 'ru-hexlet-io-courses_file/ru-hexlet-io-assets-application.css'
 
 
-def test_download(read_file,
-                  read_file_css,
-                  read_file_js,
-                  read_file_img,
-                  requests_mock):
+def test_download_result(make_request):
     with tempfile.TemporaryDirectory() as temp:
         random_path = os.path.join(temp, correct_name)
-        requests_mock.get('https://ru.hexlet.io/courses', text=read_file, 
-                          headers={'Content-Type': 'text/html'})
-        requests_mock.get('https://ru.hexlet.io/assets/professions/nodejs.png',
-                          content=read_file_img, headers={'Content-Type': 'all'})
-        requests_mock.get('https://ru.hexlet.io/assets/application.css',
-                          content=read_file_css, headers={'content-type': 'all'})
-        requests_mock.get('https://ru.hexlet.io/courses', content=read_file_css,
-                          headers={'content-type': 'all'})
-        requests_mock.get('https://ru.hexlet.io/packs/js/runtime.js',
-                          content=read_file_js, headers={'content-type': 'all'})
-        result = download('https://ru.hexlet.io/courses', temp)
+        result = download(addres, temp)
         assert random_path == result
-        assert os.path.isdir(os.path.abspath(correct_name_dir))
-        assert os.path.isfile('ru-hexlet-io-assets-professions-nodejs.png')
+        assert os.path.exists(correct_name_dir)
+
+
+def test_download_file_contents(make_request,
+                                read_file_css,
+                                read_correct_file):
+    with tempfile.TemporaryDirectory() as temp:
+        result = download(addres, temp)
+        with open(result) as f:
+            html = f.read()
+        with open(file_name, 'rb') as f:
+            css = f.read()
+        assert read_correct_file == html
+        assert read_file_css == css
+
+
+def test_download_number_of_files(make_request):
+    with tempfile.TemporaryDirectory() as temp:
+        download(addres, temp)
+        list_file = ['ru-hexlet-io-assets-application.css',
+                     'ru-hexlet-io-assets-professions-nodejs.png',
+                     'ru-hexlet-io-packs-js-runtime.js']
+        assert os.listdir(correct_name_dir) == list_file
