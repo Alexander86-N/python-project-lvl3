c1e2d5cfdca5271eea844e045178246ae5e776e0
Aleksandr <nastyavin86@br.ru>
2022-07-04 19:20:16 +0300

    change page_loader/scripts/page_loader.py

--- page_loader/scripts/page_loader.py
+++ page_loader/scripts/page_loader.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
+import logging
 from page_loader.cli import make_cli
 from page_loader.download import download
+from page_loader.init_logger import init_logger
 
 
 def main():
+    init_logger('page_loader')
+    logger = logging.getLogger('page_loader.page_loader')
     arg_one, arg_two = make_cli()
-    print(download(arg_one, arg_two))
+    try:
+        path = download(arg_one, arg_two)
+    except Exception as err:
+        logger.error(err)
+        exit(1)
+    else:
+        print(path)
+        exit(0)
 
 
 if __name__ == '__main__':
