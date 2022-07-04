a7356ff9b15b69e9d6cc73e465bc2c000a7d75a2
Aleksandr <nastyavin86@br.ru>
2022-07-04 19:19:26 +0300

    change page_loader/download.py

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,37 +1,26 @@
 import os
-from urllib.parse import urlparse
-from page_loader.data_extraction import resource_extraction, extraction_data
+import logging
+from page_loader.data_extraction import resource_extraction
+from page_loader.data_extraction import saving_data, name_formation
+logger = logging.getLogger('page_loader.download')
 
 
 def download(url, filepath):
-    title = name_formation(url)
-    filename = os.path.join(filepath, title)
-    data = extraction_data(url)
+    change_url = name_formation(url)
+    filename = os.path.join(filepath, change_url)
 
     name_dir = name_formation(url, '_file')
-    if not os.path.exists(name_dir):
-        os.mkdir(name_dir)
+    dirname = os.path.join(filepath, name_dir)
+    if not os.path.exists(dirname):
+        try:
+            os.mkdir(dirname)
+        except OSError:
+            logger.error('The dictory was not created.')
+            raise FileNotFoundError('The dictory was not created.')
 
-    urls, text_html = resource_extraction(url, data, name_dir)
+    logger.info(f'requested url: {url}')
+    text_html = resource_extraction(url, dirname, name_dir)
+    logger.info(f'output path: {os.path.abspath(filepath)}')
+    logger.info(f'write html file: {os.path.abspath(filename)}')
     saving_data(text_html, filename)
-    for url in urls:
-        data_url = extraction_data(url['addres'])
-        saving_data(data_url, url['name'])
     return os.path.abspath(filename)
-
-
-def saving_data(data, file_name):
-    if isinstance(data, str):
-        with open(file_name, 'w') as fp:
-            fp.write(data)
-    else:
-        with open(file_name, 'wb') as fp:
-            fp.write(data)
-
-
-def name_formation(addres, suffix='.html'):
-    path_parse = urlparse(addres)
-    path = os.path.splitext(path_parse.netloc + path_parse.path)[0]
-    result = ''.join([sing if sing.isalnum() else sing.replace(sing, '-')
-                      for sing in path]) + suffix
-    return result
