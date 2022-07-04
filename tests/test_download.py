e140de0584b77d5582a9c141e983885564120be6
Aleksandr <nastyavin86@br.ru>
2022-07-04 19:20:40 +0300

    change tests/test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,12 +1,13 @@
 import tempfile
 import os
+import pytest
 from page_loader.download import download
 
 
 correct_name = 'ru-hexlet-io-courses.html'
 addres = 'https://ru.hexlet.io/courses'
-correct_name_dir = 'ru-hexlet-io-courses_file'
-file_name = 'ru-hexlet-io-courses_file/ru-hexlet-io-assets-application.css'
+name_dir = 'ru-hexlet-io-courses_file'
+file_name = 'ru-hexlet-io-assets-application.css'
 
 
 def test_download_result(make_request):
@@ -14,7 +15,7 @@ def test_download_result(make_request):
         random_path = os.path.join(temp, correct_name)
         result = download(addres, temp)
         assert random_path == result
-        assert os.path.exists(correct_name_dir)
+        assert os.path.exists(os.path.join(temp, name_dir))
 
 
 def test_download_file_contents(make_request,
@@ -22,9 +23,10 @@ def test_download_file_contents(make_request,
                                 read_correct_file):
     with tempfile.TemporaryDirectory() as temp:
         result = download(addres, temp)
+        filename = os.path.join(os.path.join(temp, name_dir), file_name)
         with open(result) as f:
             html = f.read()
-        with open(file_name, 'rb') as f:
+        with open(filename, 'rb') as f:
             css = f.read()
         assert read_correct_file == html
         assert read_file_css == css
@@ -33,7 +35,13 @@ def test_download_file_contents(make_request,
 def test_download_number_of_files(make_request):
     with tempfile.TemporaryDirectory() as temp:
         download(addres, temp)
-        list_file = ['ru-hexlet-io-packs-js-runtime.js',
+        list_file = ['ru-hexlet-io-assets-application.css',
                      'ru-hexlet-io-assets-professions-nodejs.png',
-                     'ru-hexlet-io-assets-application.css']
-        assert os.listdir(correct_name_dir) == list_file
+                     'ru-hexlet-io-packs-js-runtime.js']
+        assert os.listdir(os.path.join(temp, name_dir)) == list_file
+
+
+def test_download_exceptions():
+    with tempfile.TemporaryDirectory() as temp:
+        with pytest.raises(ConnectionError):
+            download('', temp)
