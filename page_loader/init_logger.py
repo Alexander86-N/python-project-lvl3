e8b98b7eea1ca7362300e3002a47461966a1ea5b
Aleksandr <nastyavin86@br.ru>
2022-07-07 23:37:36 +0300

    change page_loader/init_logger.py

--- page_loader/init_logger.py
+++ page_loader/init_logger.py
@@ -1,16 +1,18 @@
 import logging
 
 
-def init_logger(name):
-    logger = logging.getLogger(name)
-    FORMAT = '%(asctime)s - %(name)s:%(lineno)s - %(levelname)s - %(message)s'
-    FORMAT_CONSOLE = '%(levelname)s : %(name)s : %(message)s'
-    logger.setLevel(logging.DEBUG)
-    sh = logging.StreamHandler()
-    sh.setFormatter(logging.Formatter(FORMAT_CONSOLE))
-    sh.setLevel(logging.INFO)
-    fh = logging.FileHandler(filename='./logger.log')
-    fh.setFormatter(logging.Formatter(FORMAT))
-    fh.setLevel(logging.DEBUG)
-    logger.addHandler(sh)
-    logger.addHandler(fh)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+sh = logging.StreamHandler()
+FORMAT_CONSOLE = '%(levelname)s : %(name)s : %(message)s'
+sh.setFormatter(logging.Formatter(FORMAT_CONSOLE))
+sh.setLevel(logging.INFO)
+
+fh = logging.FileHandler(filename='./logger.log')
+FORMAT = '%(asctime)s - %(name)s:%(lineno)s - %(levelname)s - %(message)s'
+fh.setFormatter(logging.Formatter(FORMAT))
+fh.setLevel(logging.DEBUG)
+
+logger.addHandler(sh)
+logger.addHandler(fh)
