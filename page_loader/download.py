e8296c4580f2418160752df6c60a24b32f2e9b23
Aleksandr <nastyavin86@br.ru>
2022-07-08 20:58:51 +0300

    Добавил документацию в файл page_loader/download.py

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,25 +1,27 @@
 import os
 from page_loader.data_extraction import resource_extraction
-from page_loader.data_extraction import saving_data, name_formation
+from page_loader.data_extraction import writes_data_file, changes_the_name
 from page_loader.init_logger import logger
 
 
 def download(url, filepath):
-    change_url = name_formation(url)
+    """Downloads the page from the network to the specified directory. 
+       Returns the full path to the downloaded file."""
+    change_url = changes_the_name(url)
     filename = os.path.join(filepath, change_url)
 
-    name_dir = name_formation(url, '_file')
+    name_dir = changes_the_name(url, '_file')
     dirname = os.path.join(filepath, name_dir)
-    if not os.path.exists(dirname):
-        try:
-            os.mkdir(dirname)
-        except FileExistsError:
-            logger.error('Folder already exists.')
-            raise FileExistsError('Folder already exists.')
+#    if not os.path.exists(dirname):
+    try:
+        os.mkdir(dirname)
+    except FileExistsError:
+        logger.error('Folder already exists.')
+        raise FileExistsError('Folder already exists.')
 
     logger.info(f'requested url: {url}')
     logger.info(f'output path: {os.path.abspath(filepath)}')
     logger.info(f'write html file: {os.path.abspath(filename)}')
     text_html = resource_extraction(url, dirname, name_dir)
-    saving_data(text_html, filename)
+    writes_data_file(text_html, filename)
     return os.path.abspath(filename)
