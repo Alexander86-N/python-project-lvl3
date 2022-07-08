548ad8ec5e4fb85aa2924b49200d49c5bcfb0e8d
Aleksandr <nastyavin86@br.ru>
2022-07-08 23:02:43 +0300

    change page_loader/download.py

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,6 +1,6 @@
 import os
-from page_loader.data_extraction import resource_extraction
-from page_loader.data_extraction import writes_data_file, changes_the_name
+from page_loader.data_extraction import resource_extraction, changes_the_name
+from page_loader.data_extraction import writes_data_file, extract_data_from_url
 from page_loader.init_logger import logger
 
 
@@ -9,7 +9,7 @@ def download(url, filepath):
        Returns the full path to the downloaded file."""
     change_url = changes_the_name(url)
     filename = os.path.join(filepath, change_url)
-
+    data = extract_data_from_url(url)
     name_dir = changes_the_name(url, '_file')
     dirname = os.path.join(filepath, name_dir)
     try:
@@ -21,6 +21,6 @@ def download(url, filepath):
     logger.info(f'requested url: {url}')
     logger.info(f'output path: {os.path.abspath(filepath)}')
     logger.info(f'write html file: {os.path.abspath(filename)}')
-    text_html = resource_extraction(url, dirname, name_dir)
+    text_html = resource_extraction(url, data, dirname, name_dir)
     writes_data_file(text_html, filename)
     return os.path.abspath(filename)
