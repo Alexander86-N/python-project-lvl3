ee577153844db3010ddeb1f8b59d31bc50ce145a
Aleksandr <nastyavin86@br.ru>
2022-07-10 19:56:13 +0300

    Change tests/test_download.py

--- tests/test_download.py
+++ tests/test_download.py
@@ -18,7 +18,7 @@ file_name = 'ru-hexlet-io-assets-application.css'
 def test_download_result(make_request):
     with tempfile.TemporaryDirectory() as temp:
         random_path = os.path.join(temp, correct_name)
-        result = download(addres, temp)
+        result = download(temp, addres)
         assert random_path == result
         assert os.path.exists(os.path.join(temp, name_dir))
 
@@ -27,7 +27,7 @@ def test_download_file_contents(make_request,
                                 read_file_css,
                                 read_correct_file):
     with tempfile.TemporaryDirectory() as temp:
-        result = download(addres, temp)
+        result = download(temp, addres)
         filename = os.path.join(os.path.join(temp, name_dir), file_name)
         with open(result) as f:
             html = f.read()
@@ -40,7 +40,7 @@ def test_download_file_contents(make_request,
 
 def test_download_number_of_files(make_request):
     with tempfile.TemporaryDirectory() as temp:
-        download(addres, temp)
+        download(temp, addres)
         assert len(os.listdir(temp)) == 2
         assert len(os.listdir(os.path.join(temp, name_dir))) == 4
 
@@ -48,19 +48,19 @@ def test_download_number_of_files(make_request):
 def test_download_exceptions():
     with tempfile.TemporaryDirectory() as temp:
         with pytest.raises(ConnectionError):
-            download('', temp)
+            download(temp, '')
 
 
 def test_download_exceptions_status_code(make_request_exceptions):
     with tempfile.TemporaryDirectory() as temp:
         with pytest.raises(Warning):
-            download(addres, temp)
+            download(temp, addres)
 
 def test_download_exception_dir():
     with tempfile.TemporaryDirectory() as temp:
         os.mkdir(os.path.join(temp, name_dir))
         with pytest.raises(FileExistsError):
-            download(addres, temp)
+            download(temp, addres)
 
 
 def test_connection_error(requests_mock):
@@ -68,15 +68,15 @@ def test_connection_error(requests_mock):
     with tempfile.TemporaryDirectory() as tmpdirname:
         assert not os.listdir(tmpdirname)
         with pytest.raises(Exception):
-            assert download(addres, tmpdirname)
+            assert download(tmpdirname, addres)
         assert not os.listdir(tmpdirname)
 
 
 def test_page_loader():
     with tempfile.TemporaryDirectory() as tmp:
-        result = subprocess.run(['page-loader', '-o', addres, tmp],
-                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        print(result.stdout)
-        print(result.stderr)
-        print(result)
+        result = subprocess.run(['page-loader', '-o', tmp, addres],
+                                encoding='utf-8',
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE)
         assert result.returncode == 0
+#        assert '' == result.stdout
