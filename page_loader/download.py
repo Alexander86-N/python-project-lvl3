14437b5553312a3864d7f86cbe79a89f84327406
Aleksandr <nastyavin86@br.ru>
2022-07-22 20:48:28 +0300

    Добавить в модуль download.py функцию download_content().

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,10 +1,10 @@
 import os
 import logging
-from page_loader.work_with_html_page import resource_extraction
-from page_loader.work_with_file_system import writes_data_file
-from page_loader.work_with_file_system import creating_directory
-from page_loader.working_with_url import extract_data_from_url
-from page_loader.working_with_url import changes_the_name
+import requests
+from progress.bar import ShadyBar
+from page_loader.html import resource_extraction
+from page_loader.storage import save, mkdir
+from page_loader.url import changes_the_name
 logger = logging.getLogger(__name__)
 
 
@@ -16,10 +16,37 @@ def download(url, filepath=os.getcwd()):
     data = extract_data_from_url(url)
     name_dir = changes_the_name(url, '_files')
     dirname = os.path.join(filepath, name_dir)
-    creating_directory(dirname)
+    mkdir(dirname)
     logger.info(f'requested url: {url}')
     logger.info(f'output path: {os.path.abspath(filepath)}')
     logger.info(f'write html file: {os.path.abspath(filename)}')
-    text_html = resource_extraction(url, data, dirname, name_dir)
-    writes_data_file(text_html, filename)
+    resources, text_html = resource_extraction(url, data, dirname, name_dir)
+    save(text_html, filename)
+    download_content(resources)
     return os.path.abspath(filename)
+
+
+def extract_data_from_url(url):
+    """Makes a request to the URL and returns its content."""
+    try:
+        response = requests.get(url)
+        logger.debug(f'File {url} received.')
+    except requests.exceptions.ConnectionError as err:
+        logger.error(f'Connection failed: {err}')
+        raise ConnectionError('Connection failed')
+    except requests.exceptions.MissingSchema:
+        logger.error('Invalid URL '': No scheme supplied.')
+        raise ConnectionError('Invalid URL '': No scheme supplied.')
+    if response.status_code != requests.codes.ok:
+        logger.error(f'Invalid request code: {response.status_code}')
+        raise Warning(f'Status_code is {response.status_code}')
+    return response.content
+
+
+def download_content(resources):
+    progres = ShadyBar('Downloading: ', max=len(resources))
+    for resource in resources:
+        data = extract_data_from_url(resource['url'])
+        save(data, resource['path'])
+        progres.next()
+    progres.finish()
