411d16e2b2d5e703c8084fc63aee61bfe3d5696f
Aleksandr <nastyavin86@br.ru>
2022-07-22 20:46:06 +0300

    Изменил название модуля storage.py

---
+++ page_loader/storage.py
@@ -0,0 +1,25 @@
+import os
+import logging
+logger = logging.getLogger(__name__)
+
+
+def save(data, file_name):
+    """Performs secure data writing to a file."""
+    mode = 'w' if isinstance(data, str) else 'wb'
+    try:
+        with open(file_name, mode) as fp:
+            fp.write(data)
+    except FileNotFoundError as err:
+        logger.error(f'File {file_name} not saved: {err}')
+        raise FileNotFoundError('File not saved')
+    logger.debug(f'File {file_name} saved.')
+
+
+def mkdir(name):
+    """Creates a directory if it does not exist."""
+    try:
+        os.mkdir(name)
+        logger.debug(f'A directory has been created: {name}')
+    except FileExistsError:
+        logger.error('Folder already exists.')
+        raise FileExistsError('Folder already exists.')
