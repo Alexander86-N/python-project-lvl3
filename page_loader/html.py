d6c3c796dcfa198c1dca5760fefb4f8d072d8599
Aleksandr <nastyavin86@br.ru>
2022-07-26 00:44:39 +0300

    Изменил название функций на get_resource() и get_links(). Изменил функцию get_resource().

--- page_loader/html.py
+++ page_loader/html.py
@@ -1,8 +1,8 @@
-import os
 import logging
+from os import path
 from bs4 import BeautifulSoup
-from urllib.parse import urlparse
-from page_loader.url import changes_the_name
+from urllib.parse import urlparse, urljoin
+from page_loader.url import to_filename
 logger = logging.getLogger(__name__)
 
 
@@ -11,35 +11,26 @@ TAGS = {'img': 'src',
         'script': 'src'}
 
 
-def resource_extraction(elementary_url, data, directory, name_dir):
+def get_resources(root_url, data, name_dir):
     """Collects local resources of the main page in the list.
        All links are replaced with links pointing to files in the directory."""
     resours = []
-    url_pars = urlparse(elementary_url)
     logger.debug('Starting resource extraction.')
     soup = BeautifulSoup(data, 'html.parser')
-    links = defines_working_links(soup, elementary_url)
+    links = get_links(soup, root_url)
     for dataset, teg in links.items():
         addres = dataset.attrs[teg]
-        value = urlparse(addres)
         logger.debug(f'Suitable link: {addres}')
-        suffix = os.path.splitext(addres)[1]
-        logger.debug(f'Suffix: {suffix}')
-        if not suffix:
-            name = changes_the_name(f'{url_pars.netloc}{addres}')
-        else:
-            name = changes_the_name(f'{url_pars.netloc}{addres}', suffix)
-        logger.debug(f'Resource name: {name}')
-        dataset.attrs[teg] = f'{name_dir}/{name}'
-        url = f'{url_pars.scheme}://{url_pars.netloc}{value.path}'
-        path = f'{directory}/{name}'
+        name = to_filename(f'{urlparse(root_url).netloc}{addres}')
+        dataset.attrs[teg] = path.join(name_dir, name)
+        url = urljoin(root_url, urlparse(addres).path)
         resours.append({'url': url,
-                        'path': path})
+                        'name': name})
     logger.debug('Resource extraction completed.')
     return resours, soup.prettify()
 
 
-def defines_working_links(data, url):
+def get_links(data, url):
     """Generates a list of local and working links."""
     url_pars = urlparse(url)
     links = {}
