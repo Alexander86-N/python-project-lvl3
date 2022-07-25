2aa4aff89dc9685ce9d5d13f1ed42f1ea2ace846
Aleksandr <nastyavin86@br.ru>
2022-07-26 00:48:00 +0300

    Добавил функции на url_parsing(), to_filename() и to_directory().

--- page_loader/url.py
+++ page_loader/url.py
@@ -1,14 +1,33 @@
+from os import path
 import re
-import os
 import logging
 from urllib.parse import urlparse
 logger = logging.getLogger(__name__)
 
 
-def changes_the_name(addres, suffix='.html'):
-    """Generates the changed address of the resource."""
-    path_parse = urlparse(addres)
-    path = os.path.splitext(path_parse.netloc + path_parse.path)[0]
-    result = re.sub(r'\W', '-', path) + suffix
-    logger.debug(f'Convert the path {addres} to a name {result}')
+def url_parsing(url):
+    """Divides the url into a path and a format."""
+    path_parse = urlparse(url)
+    addres, suffix = path.splitext(path_parse.netloc + path_parse.path)
+    return change_text(addres), suffix
+
+
+def change_text(text):
+    """Сharacters other than letters and numbers are replaced with '-'."""
+    result = re.sub(r'\W', '-', text)
+    logger.debug(f'Convert the path {text} to a name {result}')
     return result
+
+
+def to_directory(url):
+    """Forms the name of the directory."""
+    result, suffix = url_parsing(url)
+    return f'{result}_files'
+
+
+def to_filename(url):
+    """Forms the name of the file."""
+    result, suffix = url_parsing(url)
+    if suffix:
+        return f'{result}{suffix}'
+    return f'{result}.html'
